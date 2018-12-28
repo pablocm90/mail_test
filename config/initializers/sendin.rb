4af9db6c5f00f7cf78599788758c9375fa2f490c
pablo curell <pablocm90@gmail.com>
2018-12-28 15:00:34 +0100

    removed

--- config/initializers/sendin.rb
+++ config/initializers/sendin.rb
@@ -3,7 +3,7 @@ require 'sib-api-v3-sdk'
 # Setup authorization
 SibApiV3Sdk.configure do |config|
   # Configure API key authorization: api-key
-  config.api_key['api-key'] = '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'
+  config.api_key['api-key'] = ''
   # Uncomment the following line to set a prefix for the API key, e.g. 'Bearer' (defaults to nil)
   #config.api_key_prefix['api-key'] = 'Bearer'
 end
